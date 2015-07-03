0ce5c7a267053cae489bc01b10645e8c07064f0e
Ryan S <rys.DEAD2009@gmail.com>
2015-07-03 09:48:12 +0800

    Update no found team template.

--- templates/content-no-team-found.php
+++ templates/content-no-team-found.php
@@ -1,3 +1,3 @@
-<li class="no-team-found">
+<div id="people-<?php the_ID(); ?>" class="no-team-found col-12">
 	<p><?php _e( add_filter( 'no_team_found', 'No team or people found.' ), MTT_DOMAIN_NAME ); ?></p>
-</li>
+</div>
